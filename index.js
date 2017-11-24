1b0a2f677582beac2f899fbc54c382ebbafcfb13
ericpcho <ericpcho@gmail.com>
2017-11-24 11:09:53 -0800

    added models.js file for Cities and required it in index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
+const {Cities} = require('./models.js');
 
 const { PORT, CLIENT_ORIGIN } = require('./config');
 const { dbConnect } = require('./db-mongoose');
---
+++ models.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+const CitiesSchema = mongoose.Schema([{
+  city: {
+    type: String,
+    required: true,
+  },
+  state: {
+    type: String,
+    required: true
+  }
+}]);
+
+
+const Cities = mongoose.models.Cities || mongoose.model('Cities', CitiesSchema);
+
+module.exports = { Cities };
